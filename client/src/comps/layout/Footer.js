e406db52922286a31cdcfa470d4841d519d6c6da
harleauxcarrera <harleauxcarrera@gmail.com>
2018-12-20 21:06:42 -0700

    styled footer

--- client/src/comps/layout/Footer.js
+++ client/src/comps/layout/Footer.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
-import { Col } from "react-bootstrap";
+import { Col, Grid } from "react-bootstrap";
 import "../../App.css";
 class Footer extends Component {
   render() {
     return (
       <div className="footer">
-        <h1>Howdy</h1>
+        <Col lg={4} md={4} sm={4} xs={4}>
+          <h4>Howdy</h4>
+        </Col>
+        <Col lg={8} md={8} sm={8} xs={8}>
+          <h4>AudioWavez is a project made with love by Harleaux Carrera</h4>
+          2018 Scottsdale, Arizona
+        </Col>
       </div>
     );
   }
